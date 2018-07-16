8bce227cbc534f9c3bc0f908fa12a97f16728708
passion_orange <prakharsngh.1790@gmail.com>
2018-07-16 11:57:33 +0530

    Modiyfing the frame reader

--- src/main/java/passionorange/demo/KIntFrameDecoder.java
+++ src/main/java/passionorange/demo/KIntFrameDecoder.java
@@ -9,7 +9,7 @@ import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.codec.ByteToMessageDecoder;
 
 /**
- * Decodes k ints at a time.  
+ * Decodes k ints at a time.
  *
  */
 public class KIntFrameDecoder extends ByteToMessageDecoder {
@@ -21,32 +21,37 @@ public class KIntFrameDecoder extends ByteToMessageDecoder {
 
 	@Override
 	protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
-		while(in.readableBytes() >= k * Integer.SIZE/Byte.SIZE) {
-			//Transfers this buffer's data to a newly created buffer starting at readIndex
-			Integer buf = in.readInt(); 
-			out.add(buf);
-		}  
+		// check if k ints are available
+		while (in.readableBytes() >= k * Integer.SIZE / Byte.SIZE) {
+			// Transfers this buffer's data to a newly created buffer starting at readIndex
+			// read k ints at a time
+			for (int j = 0; j < k; j++) {
+				int val = in.readInt();
+				out.add(val);
+			}
+		}
 	}
-	
+
 	public static void main(String[] args) {
-		System.out.println(Integer.SIZE/Byte.SIZE);
-		//Creates a new big-endian Java heap buffer with reasonably small initial capacity, which expands its capacity boundlessly on demand.
+		// Creates a new big-endian Java heap buffer with reasonably small initial
+		// capacity, which expands its capacity boundlessly on demand.
 		ByteBuf buf = Unpooled.buffer();
-		for (int i = 0; i < 10; i++) {
-            buf.writeInt(i);
-        }
-		//Returns a buffer which shares the whole region of this buffer.
-	    //Modifying the content of the returned buffer or this buffer affects
-	    //each other's content while they maintain separate indexes and marks.
+		// try with i<10 to understand the difference
+		for (int i = 1; i <= 10; i++) {
+			buf.writeInt(i);
+		}
+		// Returns a buffer which shares the whole region of this buffer.
+		// Modifying the content of the returned buffer or this buffer affects
+		// each other's content while they maintain separate indexes and marks.
 		ByteBuf input = buf.duplicate();
-		//takes a channel inbound handler adaptor
+		// takes a channel inbound handler adaptor
 		EmbeddedChannel channel = new EmbeddedChannel(new KIntFrameDecoder(5));
-		//retain increases the reference count by 1
+		// retain increases the reference count by 1
 		channel.writeInbound(input);
 		channel.finish();
-		
+
 		Integer value;
-		while((value = channel.readInbound())!=null) {
+		while ((value = channel.readInbound()) != null) {
 			System.out.println(value + " ");
 		}
 		channel.close();
